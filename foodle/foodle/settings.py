d416167a21e08632d45f9cfef9c70d9ea360295c
Peter-Binate <peter.binate@gmail.com>
2024-10-02 12:25:57 +0200

    feat: add Ingredient model

--- foodle/foodle/settings.py
+++ foodle/foodle/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ingredients'
 ]
 
 MIDDLEWARE = [
@@ -76,17 +77,23 @@ WSGI_APPLICATION = 'foodle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# Docker
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB'),
+#         'USER': os.getenv('POSTGRES_USER'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', 'db'),
+#         'PORT': os.getenv('DB_PORT', '5432')
+#     }
+# }
 
 
 # Password validation
--- foodle/ingredients/models.py
+++ foodle/ingredients/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+from ingredients import values as ingredient_values
+
+# Create your models here.
+class Ingredient(models.Model):
+    name = models.fields.CharField(max_length=30)
+    price = models.fields.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    upload = models.ImageField(
+        upload_to ='uploads/',
+        blank=True, 
+        null=True
+    )
+    shop = models.fields.IntegerField(
+        "Nom du magasin",
+        choices=ingredient_values.SHOP_NAME,
+        default=0
+    )
+
+    def __str__(self):
+        return f'{self.name}'
